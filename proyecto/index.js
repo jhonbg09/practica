4a1a451ae5c17a9978169b4d0be3f7bfaf24c913
jhon1293 <jbetancourth@armatuvaca.com>
2025-09-07 20:23:35 -0500

    Cambiando nombre del proyecto principal

---
+++ proyecto/index.js
@@ -0,0 +1,16 @@
+class Activity {
+    constructor(id, title, description, imgUrl){
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this. imgUrl = imgUrl;
+    }
+}
+
+class Repository{
+    constructor(){
+
+    }
+
+    getAllActivities(){}
+}
