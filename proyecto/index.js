a855d923751696f10115d00de135df006a15afbd
jhon1293 <jbetancourth@armatuvaca.com>
2025-09-07 22:25:45 -0500

    Clases creadas y maquetacion tarjeta terminada

--- proyecto/index.js
+++ proyecto/index.js
@@ -8,9 +8,29 @@ class Activity {
 }
 
 class Repository{
-    constructor(){
-
+    constructor(activities = []){
+        this.activities = activities
     }
 
-    getAllActivities(){}
+    getAllActivities(){
+        return this.activities 
+    }
+
+    createActivity(id, title, description, imgUrl){
+        const newActivity = new Activity(id, title, description, imgUrl)
+        this.activities.push(newActivity)
+        return newActivity
+    }
+
+    deleteActivity(id){
+    this.activities =  this.activities.filter( (activity) => activity.id !== id )
+    }
 }
+
+const repo = new Repository();
+
+const activity1 = repo.createActivity(1, "Actividad 1", "Solucion actiidad 1", "img_url_prueba")
+const activity2 = repo.createActivity(2, "Actividad 2", "Solucion actiidad 2", "img_url_prueba")
+const activity3 = repo.createActivity(3, "Actividad 3", "Solucion actiidad 3", "img_url_prueba")
+console.log(repo.getAllActivities())
+
