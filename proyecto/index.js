a6aa3d049610a18c379e14c7f3291db7049508ed
jhon1293 <jbetancourth@armatuvaca.com>
2025-09-09 07:42:31 -0500

    creando funcion createCards

--- proyecto/index.js
+++ proyecto/index.js
@@ -1,3 +1,7 @@
+const img = document.getElementById("img");
+const titulo = document.getElementById("titulo");
+const descripcion = document.getElementById("descripcion");
+
 class Activity {
     constructor(id, title, description, imgUrl){
         this.id = id;
@@ -32,5 +36,21 @@ const repo = new Repository();
 const activity1 = repo.createActivity(1, "Actividad 1", "Solucion actiidad 1", "img_url_prueba")
 const activity2 = repo.createActivity(2, "Actividad 2", "Solucion actiidad 2", "img_url_prueba")
 const activity3 = repo.createActivity(3, "Actividad 3", "Solucion actiidad 3", "img_url_prueba")
-console.log(repo.getAllActivities())
+// console.log(repo.getAllActivities())
 
+
+
+img.innerHTML = `<img src="./assets/descarga.jpeg" alt="actividad">`
+titulo.innerHTML = "Actividad 1"
+descripcion.innerHTML = "Desarrollo Actividad 1"
+
+function createCards(Activity){
+    const {id, title, description, imgUrl} = Activity
+    const cards = repo.getAllActivities()
+    console.log(cards)
+    for(let i = 0; cards.length > i; i++){
+        
+    }
+}
+
+cards()
